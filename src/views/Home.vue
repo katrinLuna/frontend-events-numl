cbf1b76d6f8a4ddcf21d1cdd3c4b5a0797d5be96
katrinLuna <kate.nik2017@yandex.ru>
2020-03-03 17:05:24 +0300

    add past-future range btn and showing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,27 @@
 <template>
   <div class='home'>
-    <img alt='Vue logo' src='../assets/logo.png' />
+    <!-- <img alt='Vue logo' src='../assets/logo.png' /> -->
     <!-- <HelloWorld msg='Welcome to Your Vue.js App' /> -->
+    <button type="button" v-on:click="type = 'future'">Будущие</button>
+    <button type="button" v-on:click="type = 'past'">Прошедшие</button>
     <ul class='events-list' id='events'>
       <li class='events-item' v-for='event in events' :key='event.uid'>
         <p class='events-item__name'>{{ event.summary }}</p>
         <p class='events-item__date'>Дата проведения: {{ getFormattedDate(event.start) }}
-          <span v-if="getFormattedDate(event.start) !== getFormattedDate(event.start)">
-            до {{ getFormattedDate(event.end) }}</span></p>
+          <span v-if="getFormattedDate(event.start) !== getFormattedDate(event.end)">
+            до {{ getFormattedDate(event.end) || 'ночи' }} </span>
+        </p>
         <p>
-          Время проведения: c {{ getFormattedTime(event.start) }}
-          до {{ getFormattedTime(event.end) }}
+          Время проведения:
+          <span v-if="!event.allDay"> c {{ getFormattedTime(event.start) }}
+            до {{ getFormattedTime(event.end) }} </span>
+          <span v-if="event.allDay">весь день</span>
         </p>
         <p class='events-item__location'>г. {{ event.location }}</p>
         <a class='events-item__orgs' :href='event.description' alt='' target="_blank">Подробнее</a>
       </li>
     </ul>
   </div>
-
-  <!-- https://web-standards.ru/calendar.json для fetch -->
-
-  <!-- 'uid': '2020-02-29-ya-love-frontend',
-    'start': '2020-02-29T08:00:00.000Z',
-    'end': '2020-02-29T17:00:00.000Z',
-    'summary': 'Я ❤︎ Фронтенд 2020',
-    'location': 'Москва',
-  'description': 'https://ilovefrontend.ru/'-->
 </template>
 
 <script>
@@ -39,64 +35,13 @@ export default {
     // HelloWorld,
   },
   data() {
+    // eventsPeriod: future,
     return {
-      events: [
-        {
-          uid: '2020-02-29-ya-love-frontend',
-          start: '2020-02-29T08:00:00.000Z',
-          end: '2020-02-29T17:00:00.000Z',
-          summary: 'Я ❤︎ Фронтенд 2020',
-          location: 'Москва',
-          description: 'https://ilovefrontend.ru/',
-        },
-        {
-          uid: '2020-03-05-developer-circles',
-          start: '2020-03-05T15:30:00.000Z',
-          end: '2020-03-05T18:30:00.000Z',
-          summary: 'Developer Circles',
-          location: 'Москва',
-          description:
-            'https://facebook-developer-circle-moscow-front-end.splashthat.com/',
-        },
-        {
-          uid: '2020-03-14-js-fwdays',
-          start: '2020-03-14T00:00:00.000Z',
-          end: '2020-03-15T00:00:00.000Z',
-          summary: 'JavaScript fwdays',
-          location: 'Киев',
-          description: 'https://fwdays.com/en/event/js-fwdays-2020',
-          allDay: true,
-        },
-        {
-          uid: '2020-03-28-kharkivcss',
-          start: '2020-03-28T08:00:00.000Z',
-          end: '2020-03-28T17:00:00.000Z',
-          summary: 'KharkivCSS #5',
-          location: 'Харьков',
-          description: 'http://kharkivcss.org/',
-        },
-        {
-          uid: '2020-04-10-holyjs-piter',
-          start: '2020-04-10T00:00:00.000Z',
-          end: '2020-04-12T00:00:00.000Z',
-          summary: 'HolyJS Piter',
-          location: 'Санкт-Петербург',
-          description: 'https://holyjs-piter.ru/',
-          allDay: true,
-        },
-        {
-          uid: '2020-04-21-smashingconf-san-francisco',
-          start: '2020-04-21T00:00:00.000Z',
-          end: '2020-04-23T00:00:00.000Z',
-          summary: 'SmashingConf',
-          location: 'Сан-Франциско',
-          description: 'https://smashingconf.com/sf-2020/',
-          allDay: true,
-        },
-      ],
+      pastEvents: [],
+      futureEvents: [],
+      type: 'future',
     };
   },
-  eve: [],
   async mounted() {
     const rawData = await EventsService.getAll();
     const data = await rawData.json();
@@ -110,7 +55,13 @@ export default {
         futureEvents.push(element);
       }
     });
-    console.log(futureEvents[1].end + futureEvents[1].start);
+    this.futureEvents = futureEvents;
+    this.pastEvents = pastEvents.reverse();
+  },
+  computed: {
+    events() {
+      return this.type === 'future' ? this.futureEvents : this.pastEvents;
+    },
   },
   methods: {
     getFormattedDate(rawDate) {
